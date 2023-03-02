3acf6f4ad3d12d2040fc704f68570ee9e4f913d3
GGG <GGG@DESKTOP-8N3GG4O>
2023-03-02 16:48:49 +0900

    dd

--- union2/src/view/GroceryInput.java
+++ union2/src/view/GroceryInput.java
@@ -20,6 +20,8 @@ import model.F_Dao;
 import net.sourceforge.jdatepicker.impl.JDatePanelImpl;
 import net.sourceforge.jdatepicker.impl.JDatePickerImpl;
 import net.sourceforge.jdatepicker.impl.UtilDateModel;
+import java.awt.Color;
+import javax.swing.SwingConstants;
 
 public class GroceryInput extends JFrame implements ActionListener {
 	
@@ -36,7 +38,11 @@ public class GroceryInput extends JFrame implements ActionListener {
 	UtilDateModel model, model1;
 	String id;
 
+	/**
+	 * @wbp.parser.constructor
+	 */
 	public GroceryInput(MainTable01 mainTable) {
+		getContentPane().setBackground(new Color(255, 255, 255));
 		this.mainTable01 = mainTable;
 		String id = mainTable01.id;
 		System.out.println(id);
@@ -62,16 +68,22 @@ public class GroceryInput extends JFrame implements ActionListener {
 		Vector<String> large = fao.getLarge();
 		
 		large_classific = new JComboBox (large);
+		large_classific.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 13));
+		large_classific.setBackground(new Color(250, 250, 250));
 		large_classific.setToolTipText("대분류");
 		large_classific.setBounds(12, 44, 155, 40);
 		getContentPane().add(large_classific);
 		
 		middle_classific = new JComboBox(new String [] {"중분류"});
+		middle_classific.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 13));
+		middle_classific.setBackground(new Color(250, 250, 250));
 		middle_classific.setToolTipText("중분류");
 		middle_classific.setBounds(177, 44, 155, 40);
 		getContentPane().add(middle_classific);
 
 		small_classific = new JComboBox(new String [] {"소분류"});
+		small_classific.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 13));
+		small_classific.setBackground(new Color(250, 250, 250));
 		small_classific.setToolTipText("소분류");
 		small_classific.setBounds(12, 94, 155, 40);
 		getContentPane().add(small_classific);
@@ -111,110 +123,132 @@ public class GroceryInput extends JFrame implements ActionListener {
 		
 		
 		storage_place = new JComboBox(new String [] {"실온", "냉장", "냉동"});
+		storage_place.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 13));
+		storage_place.setBackground(new Color(250, 250, 250));
 		storage_place.setToolTipText("보관장소");
 		storage_place.setBounds(177, 94, 155, 40);
 		getContentPane().add(storage_place);
 
 		//세부정보
 		JLabel jlbDet = new JLabel("세부정보");
-		jlbDet.setFont(new Font("D2Coding", Font.PLAIN, 16));
-		jlbDet.setBounds(12, 149, 64, 24);
+		jlbDet.setHorizontalAlignment(SwingConstants.CENTER);
+		jlbDet.setBackground(new Color(238, 251, 255));
+		jlbDet.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 15));
+		jlbDet.setBounds(12, 149, 69, 24);
 		getContentPane().add(jlbDet);
 
 		//식자재이름
 		JLabel jlbName = new JLabel("이름");
-		jlbName.setFont(new Font("D2Coding", Font.PLAIN, 16));
-		jlbName.setBounds(12, 183, 50, 40);
+		jlbName.setHorizontalAlignment(SwingConstants.CENTER);
+		jlbName.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 15));
+		jlbName.setBounds(12, 183, 69, 40);
 		getContentPane().add(jlbName);
 		
 		groName        = new JTextField(20);
+		groName.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 13));
 		groName.setBounds(82, 183, 250, 40);
 		getContentPane().add(groName);
 		groName.setColumns(10);
 
 		//식자재수량
 		JLabel jlbQuan = new JLabel("수량");
-		jlbQuan.setFont(new Font("D2Coding", Font.PLAIN, 16));
-		jlbQuan.setBounds(12, 234, 50, 40);
+		jlbQuan.setHorizontalAlignment(SwingConstants.CENTER);
+		jlbQuan.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 15));
+		jlbQuan.setBounds(12, 234, 69, 40);
 		getContentPane().add(jlbQuan);
 
 		inQuan         = new JTextField(20);
+		inQuan.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 13));
 		inQuan.setColumns(10);
 		inQuan.setBounds(82, 234, 195, 40);
 		getContentPane().add(inQuan);
 		
 		String [] units = {"G", "KG", "ML", "L", "EA"};
 		unit = new JComboBox(units);
+		unit.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 13));
+		unit.setBackground(new Color(250, 250, 250));
 		unit.setBounds(282, 234, 50, 39);
 		getContentPane().add(unit);
 
 		//식자재가격
 		JLabel jlbPri  = new JLabel("가격");
-		jlbPri.setFont(new Font("D2Coding", Font.PLAIN, 16));
-		jlbPri.setBounds(12, 284, 50, 40);
+		jlbPri.setHorizontalAlignment(SwingConstants.CENTER);
+		jlbPri.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 15));
+		jlbPri.setBounds(12, 284, 69, 40);
 		getContentPane().add(jlbPri);
 
 		price          = new JTextField(20);
+		price.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 13));
 		price.setColumns(10);
 		price.setBounds(82, 284, 195, 40);
 		getContentPane().add(price);
 
 		JLabel jlbWon  = new JLabel("원");
-		jlbWon.setFont(new Font("D2Coding", Font.PLAIN, 18));
+		jlbWon.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 18));
 		jlbWon.setBounds(284, 284, 40, 40);
 		getContentPane().add(jlbWon);
 
 		//구매처
 		JLabel jlbSto  = new JLabel("구매처");
-		jlbSto.setFont(new Font("D2Coding", Font.PLAIN, 16));
-		jlbSto.setBounds(12, 334, 64, 40);
+		jlbSto.setHorizontalAlignment(SwingConstants.CENTER);
+		jlbSto.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 15));
+		jlbSto.setBounds(12, 334, 69, 40);
 		getContentPane().add(jlbSto);
 
 		Vector<String> storename = new Vector<String>();
 		storename = fao.getStore();
 		
 		store = new JComboBox(storename);
+		store.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 13));
+		store.setBackground(new Color(250, 250, 250));
 		store.setBounds(82, 334, 250, 40);
 		getContentPane().add(store);
 
 		//입고일
 		JLabel jlbInpD  = new JLabel("입고일");
-		jlbInpD.setFont(new Font("D2Coding", Font.PLAIN, 16));
-		jlbInpD.setBounds(12, 384, 64, 40);
+		jlbInpD.setHorizontalAlignment(SwingConstants.CENTER);
+		jlbInpD.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 15));
+		jlbInpD.setBounds(12, 384, 69, 40);
 		getContentPane().add(jlbInpD);
 		
 		model = new UtilDateModel();
 		JDatePanelImpl datePanel = new JDatePanelImpl(model);
 		JDatePickerImpl datePicker = new JDatePickerImpl(datePanel);
+		datePicker.setBackground(new Color(255, 255, 255));
 		getContentPane().add(datePicker);
 		datePicker.setBounds(82, 390, 250, 40);
 
 		//소비기한
 		JLabel jlbExpD  = new JLabel("소비기한");
-		jlbExpD.setFont(new Font("D2Coding", Font.PLAIN, 16));
-		jlbExpD.setBounds(12, 434, 64, 40);
+		jlbExpD.setHorizontalAlignment(SwingConstants.CENTER);
+		jlbExpD.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 15));
+		jlbExpD.setBounds(12, 434, 69, 40);
 		getContentPane().add(jlbExpD);
 		
 		model1 = new UtilDateModel();
 		JDatePanelImpl datePanel1 = new JDatePanelImpl(model1);
 		JDatePickerImpl datePicker1 = new JDatePickerImpl(datePanel1);
+		datePicker1.setBackground(new Color(255, 255, 255));
 		getContentPane().add(datePicker1);
 		datePicker1.setBounds(82, 440, 250, 40);
 		
 		//입력/취소버튼
 		btnInput  = new JButton("입력");
-		btnInput.setFont(new Font("D2Coding", Font.PLAIN, 18));
+		btnInput.setBackground(new Color(238, 251, 255));
+		btnInput.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 15));
 		btnInput.setBounds(70, 610, 90, 40);
 		getContentPane().add(btnInput);
 
 		JButton btnCancel = new JButton("취소");
-		btnCancel.setFont(new Font("D2Coding", Font.PLAIN, 18));
+		btnCancel.setBackground(new Color(238, 251, 255));
+		btnCancel.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 15));
 		btnCancel.setBounds(182, 610, 90, 40);
 		getContentPane().add(btnCancel);
 		
 		JLabel jlbExpD_1 = new JLabel("메모");
-		jlbExpD_1.setFont(new Font("D2Coding", Font.PLAIN, 16));
-		jlbExpD_1.setBounds(12, 484, 64, 40);
+		jlbExpD_1.setHorizontalAlignment(SwingConstants.CENTER);
+		jlbExpD_1.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 15));
+		jlbExpD_1.setBounds(12, 484, 69, 40);
 		getContentPane().add(jlbExpD_1);
 		
 		memo = new JTextField(10);
